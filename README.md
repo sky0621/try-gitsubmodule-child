f3052a37aeef05ebe4c4ad9671d796128fcf9a93
sky0621 <sky14work@gmail.com>
2018-11-02 09:07:16 +0900

    try

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # try-gitsubmodule-child
+
+$ mvn archetype:generate -Dappengine-version=1.9.67 -Djava8=true -Dfilter=com.google.appengine.archetypes:
+6
+
---
+++ try-gitsubmodule-child/src/main/java/gitsubmodule/child/util/Message.java
@@ -0,0 +1,7 @@
+package gitsubmodule.child.util;
+
+public class Message {
+    public static String hello(String msg) {
+        return "Hello, " + msg;
+    }
+}
